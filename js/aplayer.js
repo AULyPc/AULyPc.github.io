df49d19154b6555e46c53d155e7569806f115e17
AULyPc <84506514+AULyPc@users.noreply.github.com>
2024-04-12 16:32:19 +0800

    Add files via upload

--- js/aplayer.js
+++ js/aplayer.js
@@ -3,22 +3,32 @@
     window.onload = function () {
       oldLoadAp && oldLoadAp();
   
-      new APlayer({
+      const ap = new APlayer({
         container: document.getElementById('aplayer'),
         fixed: true,
         autoplay: false,
         loop: 'all',
         order: 'random',
         theme: '#b7daff',
+        listFolded: false,
+        mutex: true,
         preload: 'none',
         audio: [
           {
             name: 'ギプソフィラを花束に',
             artist: '仲村芽衣子',
             url: '/songs/仲村芽衣子 - ギプソフィラを花束に.mp3',
-            cover: '/songs/仲村芽衣子 - ギプソフィラを花束に.png'
-          }
+            cover: '/songs/仲村芽衣子 - ギプソフィラを花束に.png',
+          },
+          {
+            name: 'freesia',
+            artist: 'necofy',
+            url: '/songs/freesia - necofy.mp3',
+            cover: '/songs/freesia - necofy.jpg'
+          },
         ]
       });
     }
   })();
+
+  
